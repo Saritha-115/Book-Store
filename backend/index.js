6b3e93ee3ad41a1ce3174a91c323e66e9a69eb8a
Saritha Themiyadasa <srththemiyadasa@gmail.com>
2024-12-28 01:29:32 +0530

    added cors middleware

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,28 @@ import { PORT, mangoDBURL } from "./config.js";
 import mongoose from "mongoose";
 // import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("routed successfully");
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
 });
 
 app.use("/books", booksRoute);
